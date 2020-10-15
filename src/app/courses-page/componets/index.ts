7bda4210ef48aa72f74f6df541845c71b11f41c2
Bozhyk Maria <Maria_Bozhyk@epam.com>
2020-10-15 20:30:43 +0300

    task 1 (delete components folder and index file from courses-page folder)

--- src/app/courses-page/componets/index.ts
+++
@@ -1,4 +0,0 @@
-export { AddCourseButtonComponent } from './add-course-button';
-export { CourseComponent } from './course';
-export { CourseSearchComponent } from './course-search';
-export { CoursesListComponent } from './courses-list';
--- src/app/courses-page/courses-page.module.ts
+++ src/app/courses-page/courses-page.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesPageComponent } from './courses-page.component';
-import {
-  AddCourseButtonComponent,
-  CourseComponent,
-  CourseSearchComponent,
-  CoursesListComponent
-} from './componets';
+import { AddCourseButtonComponent } from './add-course-button';
+import { CourseComponent } from './course';
+import { CourseSearchComponent } from './course-search';
+import { CoursesListComponent } from './courses-list';
 import { BreadcrumbsModule } from '@shared/components/breadcrumbs';
 import { HeaderModule } from '@shared/components/header';
 import { LogOffButtonModule } from '@shared/components/log-off-button';
